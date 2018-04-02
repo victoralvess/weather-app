3c2b5bd94f7efb96c5a2dc851da407b2dbbc748d
Victor Alves <vsilva.programador@gmail.com>
2018-04-02 12:25:01 -0300

    Fetching data in 'App.js'

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
 
 import ForecastsList from './ForecastsList';
+import Loader from './Loader';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <ForecastsList />
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			isFetchingData: true,
+			forecasts: null
+		};
+	}
+
+	async componentDidMount() {
+		const locationResponse = await fetch(
+			'https://location.services.mozilla.com/v1/geolocate?key=test'
+		);
+		const position = await locationResponse.json();
+		const { lat, lng } = position.location;
+		const forecastsResponse = await fetch(
+			`//localhost:4000/api/v1/json?lat=${lat}&lng=${lng}&units=si`
+		);
+		const data = await forecastsResponse.json();
+		this.setState({
+			forecasts: data.daily.data,
+			isFetchingData: false
+		});
+	}
+
+	render() {
+		return (
+			<div className="container">
+				{this.state.isFetchingData ? (
+					<Loader />
+				) : (
+					<ForecastsList forecasts={this.state.forecasts} />
+				)}
+			</div>
+		);
+	}
 }
 
 export default App;
